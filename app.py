241cfdf9f8c9a3251f9d6447988c09a267509cf5
Pooja-Arumugam <poojaedumail@gmail.com>
2025-06-28 03:55:42 -0400

    Files Upload

---
+++ app.py
@@ -0,0 +1,71 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
+import pickle
+
+#Load the trained model
+model = tf.keras.models.load_model('model.h5')
+
+# Load all the pkl files:
+
+with open('label.pkl','rb') as file:
+    label_encoder_gender = pickle.load(file)
+
+with open('onehot.pkl','rb') as file:
+    onehotencodergeo = pickle.load(file)
+
+with open('scaler.pkl','rb') as file:
+    scaler = pickle.load(file)
+
+
+## streamlit app
+
+st.title("Customer Churn Prediction")
+
+## User Input
+
+geography = st.selectbox('Geography',onehotencodergeo.categories_[0])
+gender = st.selectbox('Gender',label_encoder_gender.classes_)
+age = st.slider('Age',18,90)
+balance = st.number_input('Balance')
+credit_score = st.number_input('Credit Score')
+estimated_salary = st.number_input('Estimated Salary')
+tenure = st.slider('Tenure',0,10)
+num_of_products = st.slider('Number of Products',1,4)
+has_cr_card= st.selectbox('Has Credit Card',[0,1])
+is_active_member = st.selectbox('Is Active Member',[0,1])
+
+# Prepare input data
+input_data = pd.DataFrame({
+    'CreditScore': [credit_score],
+    'Gender':[label_encoder_gender.transform([gender])[0]],
+    'Age' : [age],
+    'Tenure' : [tenure],
+    'Balance' : [balance],
+    'NumOfProducts': [num_of_products],
+    'HasCrCard':[has_cr_card],
+    'IsActiveMember' : [is_active_member],
+    'EstimatedSalary': [estimated_salary]
+})
+
+# One hot encoder 'Geography'
+geo_encoded = onehotencodergeo.transform([[geography]]).toarray()
+geo_encoded_df = pd.DataFrame(geo_encoded,columns = onehotencodergeo.get_feature_names_out(['Geography']))
+
+# combine the data
+input_data = pd.concat([input_data.reset_index(drop=True),geo_encoded_df],axis=1)
+
+input_data_scaled = scaler.transform(input_data)
+
+prediction = model.predict(input_data_scaled)
+
+prediction_prob = prediction[0][0]
+
+st.write(f"The Churn Probability is:{prediction_prob: .2f}")
+
+if prediction_prob>0.5:
+    st.write("The customer is likely to Churn")
+else:
+    st.write("The customer is not likely to Churn")
